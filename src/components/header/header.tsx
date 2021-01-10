b46ac5e931043fd2c29f073867e0790ddac44b87
maciej778 <maciej778TEST@gmail.com>
2021-01-10 20:15:49 +0100

    change title of header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import './header.scss';
 const Header = () => {
     return (
         <header className="header">
-            <h2 className="header__h2">Kind of a mind map</h2>
+            <h2 className="header__h2">Mind map</h2>
         </header>
     )
 }
